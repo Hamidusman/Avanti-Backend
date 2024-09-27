a57b808226e6893a7c0028faf040874dc7d8ee73
hamidusman <abdulhamidusman218@gmail.com>
2024-09-27 23:56:04 +0100

    chaned booking url

--- apps/hotel/urls.py
+++ apps/hotel/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+
+router = DefaultRouter()
+
+router.register('booking', views.Book, basename='booking')
+
 urlpatterns = [
     path('hotel', views.HotelView.as_view()),
     path('hotel/<int:id>', views.HotelDetail.as_view()),
     path('ratings', views.RatingView.as_view()),
-    path('booking/', views.Book.as_view({'get': 'list'}))
+    path('', include(router.urls))
     
 ]
